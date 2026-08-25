338a47e1ad33463da7280831db8feec5a9f1ca6c
deepthihegdekkl <deepthihegdekkl@gmail.com>
2026-08-25 19:13:28 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxi=0
+        mini=prices[0]
+        for i in range(len(prices)):
+            if prices[i]<mini:
+                mini=prices[i]
+            maxi=max(maxi,prices[i]-mini)
+        return maxi
+        
+        
